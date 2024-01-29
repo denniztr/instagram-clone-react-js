44dd1567679c7198d167478528b38911d7b77986
denniztr <denniztr@gmail.com>
2024-01-29 20:49:04 +0300

    register user action completed, with no user avatar

--- src/components/auth/ui/form/form.jsx
+++ src/components/auth/ui/form/form.jsx
@@ -1,9 +1,22 @@
 import {useState} from "react";
 import {Link} from "react-router-dom";
+import {useRegisterUserMutation} from "../../../../store/api/index.js";
+
 import * as Styled from './form.styles.js'
 
 export const AuthForm = () => {
     const [isRegister, setIsRegister] = useState(false)
+    const [name, setName] = useState(null)
+    const [login, setLogin] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    const [registerUser] = useRegisterUserMutation()
+
+    const handleRegister = (event) => {
+        event.preventDefault()
+        registerUser({name, login, password})
+            .then(res => console.log(res))
+    }
 
     return (
         <Styled.FormContainer>
@@ -12,11 +25,26 @@ export const AuthForm = () => {
             </Link>
             {isRegister ? (
                 <Styled.Form>
-                    <input type="text" placeholder="Name"/>
-                    <input type="text" placeholder="login"/>
-                    <input type="password" placeholder="password"/>
-                    <input type="file"/>
-                    <Styled.Button>Create account</Styled.Button>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="login"
+                        onChange={(event) => setLogin(event.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <input
+                        type="file"
+                        onChange={(event) => setImage(event.target.files[0])}
+                    />
+                    <Styled.Button onClick={(event) => handleRegister(event)}>Create account</Styled.Button>
                     <Styled.FormText>Already have an account?<span onClick={() => setIsRegister(false)}> Log in. </span></Styled.FormText>
                 </Styled.Form>
             ) : (
--- src/store/api/auth-api/auth-api.js
+++ src/store/api/auth-api/auth-api.js
@@ -3,19 +3,19 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const authApi = createApi({
     reducerPath: 'auth-api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://wedev-api.sky.pro/api/v1/prod/instapro'
+        baseUrl: 'https://wedev-api.sky.pro/'
     }),
     endpoints: (build) => ({
         registerUser: build.mutation({
             query: (body) => ({
                 url: 'api/user',
                 method: 'POST',
-                body: {
+                body: JSON.stringify({
                     login: body.login,
-                    password: body.password,
                     name: body.name,
-                    imageUrl: body.imageUrl,
-                },
+                    password: body.password,
+                    // imageUrl: body.imageUrl,
+                }),
             }),
         }),
     }),
