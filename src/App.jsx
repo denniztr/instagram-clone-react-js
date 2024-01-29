3af8087f5ed703f21a8c2979318c050211baa08a
denniztr <denniztr@gmail.com>
2024-01-29 13:16:54 +0300

    store init, api init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {BrowserRouter} from "react-router-dom"
+import {Provider} from "react-redux";
 import {AppRouter} from './router.jsx'
+import {store} from './store/index.js'
 import {GlobalStyles} from './global/index.js'
 
 function App() {
   return (
-      <BrowserRouter>
-          <GlobalStyles />
-          <AppRouter />
-      </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <GlobalStyles />
+              <AppRouter />
+          </BrowserRouter>
+      </Provider>
   )
 }
 
--- src/components/content/ui/card/card.jsx
+++ src/components/content/ui/card/card.jsx
@@ -1,145 +1,49 @@
 import PropTypes from "prop-types";
-import pic from './man.jpeg'
-import pic1 from './pic2.jpg'
-import pic2 from './pic3.jpg'
-import pic3 from './pıc4.jpg'
-import pic4 from './pic5.jpg'
-import pic6 from './signer.jpg'
-import pic7 from './pic8.jpg'
-
 import { FaRegHeart } from "react-icons/fa";
 import { LuMessageSquare } from "react-icons/lu";
-
 import * as Styled from './card.styles.js'
 
 
-export const Card = () => {
+export const Card = props => {
+    const {
+        imageUrl: img,
+        isLiked: liked,
+        user: {
+            imageUrl: userImgUrl,
+            login: userLogin,
+            name: userName,
+        }
+    } = props
 
-  return (
-      <>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic} alt=""/>
-              </Styled.Image>
-              <Styled.Footer>
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/100`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic1} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/101`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic2} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/102`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic3} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/103`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic4} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/104`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic6} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/105`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-          <Styled.Card>
-              <Styled.Image>
-                  <img src={pic7} alt=""/>
-              </Styled.Image>
-              <Styled.Footer >
-                  <Styled.UserInfo>
-                      <Styled.UserPhoto>
-                          <img src={`https://i.pravatar.cc/106`} alt=""/>
-                      </Styled.UserPhoto>
-                      <h4>Sam Kuk</h4>
-                  </Styled.UserInfo>
-                  <Styled.CardAction>
-                      <FaRegHeart/>
-                      <LuMessageSquare />
-                  </Styled.CardAction>
-              </Styled.Footer>
-          </Styled.Card>
-      </>
-  )
+    return (
+        <Styled.Card>
+            <Styled.Image>
+                <img src={img} alt=""/>
+            </Styled.Image>
+            <Styled.Footer>
+                <Styled.UserInfo>
+                    <Styled.UserPhoto>
+                        <img src={userImgUrl} alt=""/>
+                    </Styled.UserPhoto>
+                          {/*<h4>{userName}<span>{userLogin}</span></h4>*/}
+                    <h4>{userName}</h4>
+                    <span>{userLogin}</span>
+                </Styled.UserInfo>
+                <Styled.CardAction>
+                    <FaRegHeart/>
+                    <LuMessageSquare />
+                </Styled.CardAction>
+            </Styled.Footer>
+        </Styled.Card>
+    )
 }
 
 Card.propTypes = {
-    cardStyles: PropTypes.object,
+    imageUrl: PropTypes.string,
+    isLiked: PropTypes.bool,
+    user: PropTypes.shape({
+        imageUrl: PropTypes.string,
+        login: PropTypes.string,
+        name: PropTypes.string,
+    }),
 }
